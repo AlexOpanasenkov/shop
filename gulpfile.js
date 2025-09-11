0eaaa067f2784045ff9eddd11d9f8352e5c1704a
AlexOpanasenkov <a.opanasenkov@gmail.com>
2025-09-11 13:33:12 +0200

    feat: refactoring gulpfile: added const gulp = require(gulp);

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const { src, dest, watch, series, parallel } = require('gulp');
 
-const scss = require('gulp-sass')(require('sass'));
+const scss = require('gulp-sass')(require('sass'));  // Один require для sass/scss
 const browserSync = require('browser-sync').create();
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify-es').default;
@@ -11,24 +11,23 @@ const webp = require('gulp-webp');
 const avif = require('gulp-avif');
 const newer = require('gulp-newer');
 const ttf2woff2 = require('gulp-ttf2woff2');
-const sourcemaps = require('gulp-sourcemaps');
+const sourcemaps = require('gulp-sourcemaps');  // Добавьте, если не установлен: npm i --save-dev gulp-sourcemaps
 const include = require('gulp-include');
 const svgstore = require('gulp-svgstore');
 const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
 
 gulp.task('scss', function () {
   return gulp.src('app/scss/style.scss')
-    .pipe(sass({
-      includePaths: ['app/scss'] // ← теперь можно писать @use 'vars';
-    }).on('error', sass.logError))
+    .pipe(scss({
+      includePaths: ['app/scss']  // @use 'vars';
+    }).on('error', scss.logError))
     .pipe(gulp.dest('app/css'));
 });
 
 function sprites() {
   return src('app/images/sprite/*.svg')
     .pipe(svgstore())
-    .pipe(dest('app/images'))
+    .pipe(dest('app/images'));
 }
 
 function pages() {
@@ -37,46 +36,50 @@ function pages() {
       includePaths: 'app/components'
     }))
     .pipe(dest('app'))
-    .pipe(browserSync.stream())
+    .pipe(browserSync.stream());
 }
 
 function fonts() {
   return src('app/fonts/*.ttf')
     .pipe(ttf2woff2())
-    .pipe(dest('app/fonts'))
+    .pipe(dest('app/fonts'));
 }
 
-
 function images() {
-  return src(['app/images/src/*.*', '!app/images/src/*.svg'])
-    .pipe(newer('app/images'))
-    .pipe(avif({ quality: 50 }))
+  // Исправление: Один src, затем ветвление для форматов (используем filter для избежания перезаписи)
+  const imageSrc = ['app/images/src/*.*', '!app/images/src/*.svg'];  // Исключаем SVG
 
-    .pipe(src('app/images/src/*.*'))
+  return src(imageSrc)
+    .pipe(newer('app/images'))
+    // AVIF
+    .pipe(avif({ quality: 50 }))
+    .pipe(dest('app/images'))  // Сохраняем AVIF
+    // WebP (на оригиналах)
+    .pipe(src(imageSrc))  // Новый stream для WebP
     .pipe(newer('app/images'))
     .pipe(webp())
-
-    .pipe(src('app/images/src/*.*'))
+    .pipe(dest('app/images'))
+    // Оригиналы оптимизированные
+    .pipe(src(imageSrc))
     .pipe(newer('app/images'))
     .pipe(imagemin())
-
-    .pipe(dest('app/images'))
+    .pipe(dest('app/images'));
 }
 
 function styles() {
-  console.log('Запуск задачи styles'); // Добавьте эту строку
+  console.log('Запуск задачи styles');
   return src('app/scss/style.scss')
-  .pipe(sourcemaps.init())
+    .pipe(sourcemaps.init())  // Перемещено ДО scss() для корректных карт
     .pipe(scss({
-      outputStyle: 'expanded',
-      includePaths: ['app/scss'] // теперь @use 'vars'; будет работать
+      outputStyle: 'expanded',  // Для prod: 'compressed'
+      includePaths: ['app/scss']
     }).on('error', scss.logError))
     .pipe(autoprefixer({
-      overrideBrowserslist: ['last 10 versions'],
+      overrideBrowserslist: ['last 10 versions'],  // Современный browserslist
       cascade: false
     }))
     .pipe(concat('style.min.css'))
-    .pipe(sourcemaps.write('.')) // Добавьте эту строку
+    .pipe(sourcemaps.write('.'))  // Генерирует .map файлы
     .pipe(dest('app/css'))
     .pipe(browserSync.stream());
 }
@@ -87,10 +90,12 @@ function scripts() {
     'node_modules/nouislider/dist/nouislider.js',
     'app/js/main.js'
   ])
+    .pipe(sourcemaps.init())  // Добавлено: sourcemaps для JS
     .pipe(concat('main.min.js'))
     .pipe(uglify())
+    .pipe(sourcemaps.write('.'))  // Добавлено: write maps
     .pipe(dest('app/js'))
-    .pipe(browserSync.stream())
+    .pipe(browserSync.stream());
 }
 
 function watching() {
@@ -99,37 +104,29 @@ function watching() {
       baseDir: 'app/'
     }
   });
-  watch(['app/scss/**/*.scss'], styles) // Следим за всеми SCSS-файлами в подпапках
-  // watch(['app/scss/*.scss'], styles)
-  // watch(['app/images/src//**/*'], images)
-  watch('app/images/src/**/*.{png,jpg,jpeg,webp,avif,gif,svg}', images);
-  watch(['app/images/sprite'], sprites)
-  watch(['app/pages/*', 'app/components/*'], pages)
-  watch(['app/js/main.js'], scripts)
-  watch(['app/*.html']).on('change', browserSync.reload)
+  watch(['app/scss/**/*.scss'], styles);
+  watch('app/images/src/**/*.{png,jpg,jpeg,gif}', images);  // Убрал svg из watch, т.к. исключаем
+  watch(['app/images/sprite/*.svg'], sprites);  // Уточнил glob
+  watch(['app/pages/*', 'app/components/*'], pages);
+  watch(['app/js/main.js'], scripts);
+  watch(['app/*.html']).on('change', browserSync.reload);
 }
-
-// function cleanDist() {
-//   return src('dist')
-//   .pipe(clean())
-// }
 
 function cleanDist() {
   return src('dist', { allowEmpty: true })
-    .pipe(clean({ force: true }))
+    .pipe(clean({ force: true }));
 }
 
 function building() {
   return src([
-    'app/**/*.html',        // все HTML (включая из pages, если они уже собраны в app/)
-    'app/css/**/*.css',     // все css
-    'app/js/**/*.js',       // все js
-    'app/images/**/*.*',    // все картинки + подпапки
-    'app/fonts/**/*.*'      // все шрифты
+    'app/**/*.html',
+    'app/css/**/*.css',
+    'app/js/**/*.js',
+    'app/images/**/*.*',
+    'app/fonts/**/*.*'
   ], { base: 'app' })
   .pipe(dest('dist'));
 }
-
 
 exports.styles = styles;
 exports.watching = watching;
@@ -141,6 +138,5 @@ exports.pages = pages;
 exports.cleanDist = cleanDist;
 exports.building = building;
 
-exports.build = series(cleanDist, building);
-exports.default = parallel(styles, images, sprites, scripts, pages, watching);
-
+exports.build = series(cleanDist, parallel(styles, images, sprites, scripts, pages), building);  // Добавил parallel для задач перед копированием
+exports.default = watching;  // Только dev, без build
